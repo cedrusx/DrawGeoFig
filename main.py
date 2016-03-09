0216b38fb611783d69b2858b13282ca1292700ee
cedrusx <shixuesong@gmail.com>
2016-03-09 23:44:06 +0800

    Support underdetermined cases and bug fixing
    By implementing random decision of positions, it now can deal with
    all cases of freedom.
    
    Other changes:
    1. Use logging instead of print, and reduce debug information
    2. Fix multiple bugs and enhance robustness
    3. Add some test cases
    
    Signed-off-by: cedrusx <shixuesong@gmail.com>

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import sys
 import math
+import random
 import numpy as np
 import matplotlib.pyplot as plt
+import logging
+logging.basicConfig(level=logging.DEBUG, format='%(message)s')
 
 DEF_ANGLE = '='
 DEF_PERP = '|'
@@ -9,10 +12,10 @@ DEF_PARA = '-'
 DEFAULT_POSITIONS = [(0,0), (2,0), (1,1)]
 TYPES = {'POSITION':0, 	# '(0,0)'
 		 'ANGLE':1, 	# 'ABC=60', 'ABC' (meaning 'ABC=180')
-		 'PERP':2, 		# 'AB-BC'
-		 'PARA':3,		# 'AB|CD'
+		 'PERP':2, 		# 'AB|BC'
+		 'PARA':3,		# 'AB-CD'
 		 'UNKNOWN':4}
-			
+
 def gettype(rule):
 	if ',' in rule:
 		return TYPES['POSITION']
@@ -40,6 +43,8 @@ class RuleParser:
 			newrule = newrule.rstrip(' \n').split(';')
 			dots.extend(newdot)
 			for r in newrule:
+				if r is '':
+					continue
 				if len(r) == 2:
 					lines.append(sorted(r))
 				else:
@@ -57,7 +62,6 @@ class RuleParser:
 		for new in newlines:
 			if sorted(new) not in lines:
 				lines.append(sorted(new))
-
 
 class SmartCalc:
 	"""Init me with an input stream giving rules, then I'll give you the world
@@ -90,8 +94,46 @@ class SmartCalc:
 			if self.dpcount == len(DEFAULT_POSITIONS):
 				self.dpcount -= len(DEFAULT_POSITIONS)
 		else:
-			self.dpcount == 0
-		return DEFAULT_POSITIONS(self.dpcount)
+			self.dpcount = 0
+		return DEFAULT_POSITIONS[self.dpcount]
+
+	def __getrandomposition(self):
+		"""Return a random position not too far away from current positions."""
+		cp = self.positions.values() # current positions for reference
+		if len(cp) == 0: # No reference positions
+			return (random.gauss(0, 1), random.gauss(0, 1))
+		xmin,ymin = xmax,ymax = cp[0]
+		for x,y in cp[1:]:
+			xmin = min(xmin, x)
+			ymin = min(ymin, y)
+			xmax = max(xmax, x)
+			ymax = max(ymax, y)
+		if xmin == xmax and ymin == ymax: # Only one reference position
+			return (cp[0][0] + random.gauss(0, 1),
+					cp[0][1] + random.gauss(0, 1))
+		std = max(xmax - xmin, ymax - ymin)
+		return (random.gauss((xmax + xmin) / 2, std),
+				random.gauss((ymax + ymin) / 2, std))
+
+	def __getrandompositiononline(self, line):
+		"""Return a random position on a given line."""
+		cp = self.positions.values() # current positions for reference
+		if len(cp) == 0: # No reference positions
+			px = random.gauss(0, 1)
+		else:
+			xmin,ymin = xmax,ymax = cp[0]
+			for x,y in cp[1:]:
+				xmin = min(xmin, x)
+				ymin = min(ymin, y)
+				xmax = max(xmax, x)
+				ymax = max(ymax, y)
+			if xmin == xmax and ymin == ymax: # Only one reference position
+				px = cp[0][0]
+			else:
+				std = max(xmax - xmin, ymax - ymin)
+				px = random.gauss((xmax + xmin) / 2, std)
+		py = line[0] * px + line[1]
+		return (px,py)
 
 	def __getlinefromrule(self, dot, rule):
 		"""For a dot, each rule can restrict it to be on a line.
@@ -111,7 +153,8 @@ class SmartCalc:
 				baseline = rule[1::-1]	# 'ABC' -> 'BA'
 				basedot = rule[1]
 			else:
-				print 'Invalid rule: ', rule
+				logging.error('Invalid rule: ' + rule)
+				return
 		elif type is TYPES['PERP'] or type is TYPES['PARA']:
 			if type is TYPES['PERP']:
 				rotation = 90.0
@@ -125,14 +168,16 @@ class SmartCalc:
 				baseline = rule[3:5]
 			elif rule[3] is dot:
 				basedot = rule[4]
-				baseline = rule[0:1]
+				baseline = rule[0:2]
 			elif rule[4] is dot:
 				basedot = rule[3]
-				baseline = rule[0:1]
+				baseline = rule[0:2]
 			else:
-				print 'Invalid rule: ', rule
+				logging.error('Invalid rule: ' + rule)
+				return
 		else:
-			print 'Unknown rule: ', rule
+			logging.error('Unknown rule: ', rule)
+			return
 		# Now we get baseline, basedot and rotation
 		x0,y0 = self.positions[basedot]
 		x1,y1 = self.positions[baseline[0]]
@@ -143,15 +188,15 @@ class SmartCalc:
 			elif y2 < y1:
 				theta = -math.pi/2
 			else:
-				print 'Invalid rule (identical dots): ', rule
+				logging.error('Invalid rule (identical dots): ' + rule)
+				return
 		else:
 			theta = math.atan((y2 - y1) / (x2 - x1))
-		print 'dot:', dot, 'rule:', rule
-		print baseline, 'theta =', theta / math.pi * 180, 'rotation =', rotation
 		theta += rotation / 180.0 * math.pi
 		a = math.tan(theta)
 		b = y0 - a * x0
-		print basedot + dot, 'theta =', theta / math.pi * 180, 'y=' + str(a) + 'x+' + str(b)
+		logging.info(basedot + dot + ': y=' + str(a) + 'x+' + str(b) \
+			+ ' (theta=' + str(theta / math.pi * 180) + ')')
 		return a,b
 
 	def __findintersect(self, lines):
@@ -160,8 +205,8 @@ class SmartCalc:
 		a1,b1 = lines[0]
 		a2,b2 = lines[1]
 		if (a1 == a2):
-			print 'Error! Parallel lines never intersect!'
-			return
+			logging.warning('Oops! Parallel lines never intersect! Ignoring one rule')
+			return self.__getrandompositiononline(lines[0])
 		x = (b2 - b1) / (a1 - a2)
 		y = (a1 * b2 - a2 * b1) / (a1 - a2)
 		return (x,y)
@@ -173,6 +218,7 @@ class SmartCalc:
 			freedom = 2
 			lines = []
 			rules = self.__getrulesfordot(dot)
+			logging.info(dot + ": " + str(rules))
 			for rule in rules:
 				type = gettype(rule)
 				if type is TYPES['POSITION']:
@@ -180,27 +226,27 @@ class SmartCalc:
 					pos = (float(x), float(y))
 					freedom = 0
 					if len(rules) > 1:
-						print 'Position of ', dot, ' is assigned. ', \
-							'Other rules will be ignored'
+						logging.warning('Position of ' + dot + ' is assigned. ' \
+							+ 'Other rules will be ignored')
 				else:
-					lines.append(self.__getlinefromrule(dot, rule))
-					freedom -= 1
+					line = self.__getlinefromrule(dot, rule)
+					if line is not None:
+						lines.append(line)
+						freedom -= 1
 				# If overdetermined, additional rules would be ignored
 				if freedom == 0:
 					break
 			if freedom == 2:
-				print 'Position of ', dot, ' is not defined. ', \
-					'Default position will be used.'
-				pos = self.__getdefaultposition()
+				# Not determined - generate a random position
+				pos = self.__getrandomposition()
 			elif freedom == 1:
-				# TODO: underdetermined
-				print 'Position of ', dot, ' is underdetermined.' \
-					'I don\'t know what to do'
-				pos = (0, 0)
+				# Underdetermined - generate a semi-random position 
+				pos = self.__getrandompositiononline(lines[0])
 			elif len(lines) == 2:
+				# Fully determined - calculate the exact position
 				pos = self.__findintersect(lines)
 			self.positions[dot] = pos
-			print dot + ': (' + str(pos[0]) + ',' + str(pos[1]) + ')'
+			logging.info('Freedom: ' + str(freedom) + '. Position: ' + str(pos))
 
 def main():
 	if len(sys.argv) == 2:
