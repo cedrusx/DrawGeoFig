25903f8f2558675f1edb28e59f3f0ce0c68ddf7f
cedrusx <shixuesong@gmail.com>
2016-03-12 19:09:44 +0800

    Make classes for rules #1
    Define methods & usage of rule classes.
    Rewrite RuleParser::parse to use the rule classes.
    Change tabs to spaces.
    
    Signed-off-by: cedrusx <shixuesong@gmail.com>

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import math
 import random
 import numpy as np
 import matplotlib.pyplot as plt
+import rules
 import logging
 logging.basicConfig(level=logging.DEBUG, format='%(message)s')
 
@@ -10,269 +11,262 @@ DEF_ANGLE = '='
 DEF_PERP = '|'
 DEF_PARA = '-'
 DEFAULT_POSITIONS = [(0,0), (2,0), (1,1)]
-TYPES = {'POSITION':0, 	# '(0,0)'
-		 'ANGLE':1, 	# 'ABC=60', 'ABC' (meaning 'ABC=180')
-		 'PERP':2, 		# 'AB|BC'
-		 'PARA':3,		# 'AB-CD'
-		 'UNKNOWN':4}
+TYPES = {'POSITION':0,  # '(0,0)'
+         'ANGLE':1,     # 'ABC=60', 'ABC' (meaning 'ABC=180')
+         'PERP':2,      # 'AB|BC'
+         'PARA':3,      # 'AB-CD'
+         'UNKNOWN':4}
 
 def gettype(rule):
-	if ',' in rule:
-		return TYPES['POSITION']
-	if len(rule) == 3 or DEF_ANGLE in rule:
-		return TYPES['ANGLE']
-	if DEF_PERP in rule:
-		return TYPES['PERP']
-	if DEF_PARA in rule:
-		return TYPES['PARA']
-	return TYPES['UNKNOWN']
+    if ',' in rule:
+        return TYPES['POSITION']
+    if len(rule) == 3 or DEF_ANGLE in rule:
+        return TYPES['ANGLE']
+    if DEF_PERP in rule:
+        return TYPES['PERP']
+    if DEF_PARA in rule:
+        return TYPES['PARA']
+    return TYPES['UNKNOWN']
 
-class RuleParser:
-	"""A parser to get rules from an input stream.
+class RuleParser(object):
+    """A parser to get rules from an input stream.
 
-	Attributes:
-		dots: a list of names like ['A','B']
-		lines: a list of dot pairs like [['A','B'],['B','C']]
-		dotrules: a list of dot-rule pairs
-	"""
+    Attributes:
+        dots: a list of names like ['A','B']
+        lines: a list of dot pairs like [['A','B'],['B','C']]
+        dotrules: a list of dot-rule pairs
+    """
 
-	def __init__(self):
-		self.dots = []
-		self.lines = []
-		self.dotrules = []
+    def __init__(self):
+        self.dots = []
+        self.lines = []
+        self.dotrules = []
 
-	def parse(self, input):
-		while True:
-			line = input.readline()
-			if not line:
-				break
-			newdot, newrule = line.split(':')
-			newdot = newdot.split(',')
-			newrule = newrule.rstrip(' \n').split(';')
-			self.dots.extend(newdot)
-			for r in newrule:
-				if r is '':
-					continue
-				if len(r) == 2:
-					self.__addlines([r])
-				else:
-					for d in newdot:
-						self.dotrules.append((d,r))
-					if len(r) == 3 or DEF_ANGLE in r:
-						self.__addlines([r[0:2], r[1:3]])
-					elif DEF_PERP in r:
-						self.__addlines(r.split(DEF_PERP))
-					elif DEF_PARA in r:
-						self.__addlines(r.split(DEF_PARA))
+    def parse(self, input):
+        while True:
+            line = input.readline()
+            if not line:
+                break
+            newdot, newrule = line.split(':')
+            newdots = newdot.split(',')
+            newrules = newrule.rstrip(' \n').split(';')
+            self.dots.extend(newdots)
+            for r in newrules:
+                rule_constructor = rules.RuleConstructor(r)
+                if rule_constructor.rule:
+                    for d in newdots:
+                        self.dotrules.append(
+                            (d, rule_constructor.rule.description))
+                self.__addlines(rule_constructor.shapes)
 
-	def __addlines(self, newlines):
-		"""Avoid duplication when adding a line into self.lines."""
-		for new in newlines:
-			if sorted(new) not in self.lines:
-				self.lines.append(sorted(new))
+    def __addlines(self, newlines):
+        """Avoid duplication when adding a line into self.lines."""
+        for new in newlines:
+            if sorted(new) not in self.lines:
+                self.lines.append(sorted(new))
 
 class SmartCalc(RuleParser):
-	"""Init me with an input stream giving rules, then I'll give you the world
+    """Init me with an input stream giving rules, then I'll give you the world
 
-	Attributes:
-		positions: a dict containing the coordinates of each dot
-	"""
+    Attributes:
+        positions: a dict containing the coordinates of each dot
+    """
 
-	def __init__(self, input):
-		"""All works should be done here."""
-		RuleParser.__init__(self)
-		self.parse(input)
-		self.positions = {}
-		self.__calcpositions()
+    def __init__(self, input):
+        """All works should be done here."""
+        RuleParser.__init__(self)
+        self.parse(input)
+        self.positions = {}
+        self.__calcpositions()
 
-	def __getrulesfordot(self, dot):
-		"""Return all rules for a specified dot."""
-		rules = []
-		for r in self.dotrules:
-			if r[0] == dot:
-				rules.append(r[1])
-		return rules
+    def __getrulesfordot(self, dot):
+        """Return all rules for a specified dot."""
+        rules = []
+        for r in self.dotrules:
+            if r[0] == dot:
+                rules.append(r[1])
+        return rules
 
-	def __getdefaultposition(self):
-		"""Return the next position from the list DEFAULT_POSITIONS."""
-		if self.__dict__.has_key('dpcount'):
-			self.dpcount += 1
-			if self.dpcount == len(DEFAULT_POSITIONS):
-				self.dpcount -= len(DEFAULT_POSITIONS)
-		else:
-			self.dpcount = 0
-		return DEFAULT_POSITIONS[self.dpcount]
+    def __getdefaultposition(self):
+        """Return the next position from the list DEFAULT_POSITIONS."""
+        if self.__dict__.has_key('dpcount'):
+            self.dpcount += 1
+            if self.dpcount == len(DEFAULT_POSITIONS):
+                self.dpcount -= len(DEFAULT_POSITIONS)
+        else:
+            self.dpcount = 0
+        return DEFAULT_POSITIONS[self.dpcount]
 
-	def __getrandomposition(self):
-		"""Return a random position not too far away from current positions."""
-		cp = self.positions.values() # current positions for reference
-		if len(cp) == 0: # No reference positions
-			return (random.gauss(0, 1), random.gauss(0, 1))
-		xmin,ymin = xmax,ymax = cp[0]
-		for x,y in cp[1:]:
-			xmin = min(xmin, x)
-			ymin = min(ymin, y)
-			xmax = max(xmax, x)
-			ymax = max(ymax, y)
-		if xmin == xmax and ymin == ymax: # Only one reference position
-			return (cp[0][0] + random.gauss(0, 1),
-					cp[0][1] + random.gauss(0, 1))
-		std = max(xmax - xmin, ymax - ymin)
-		return (random.gauss((xmax + xmin) / 2, std),
-				random.gauss((ymax + ymin) / 2, std))
+    def __getrandomposition(self):
+        """Return a random position not too far away from current positions."""
+        cp = self.positions.values() # current positions for reference
+        if len(cp) == 0: # No reference positions
+            return (random.gauss(0, 1), random.gauss(0, 1))
+        xmin,ymin = xmax,ymax = cp[0]
+        for x,y in cp[1:]:
+            xmin = min(xmin, x)
+            ymin = min(ymin, y)
+            xmax = max(xmax, x)
+            ymax = max(ymax, y)
+        if xmin == xmax and ymin == ymax: # Only one reference position
+            return (cp[0][0] + random.gauss(0, 1),
+                    cp[0][1] + random.gauss(0, 1))
+        std = max(xmax - xmin, ymax - ymin)
+        return (random.gauss((xmax + xmin) / 2, std),
+                random.gauss((ymax + ymin) / 2, std))
 
-	def __getrandompositiononline(self, line):
-		"""Return a random position on a given line."""
-		cp = self.positions.values() # current positions for reference
-		if len(cp) == 0: # No reference positions
-			px = random.gauss(0, 1)
-		else:
-			xmin,ymin = xmax,ymax = cp[0]
-			for x,y in cp[1:]:
-				xmin = min(xmin, x)
-				ymin = min(ymin, y)
-				xmax = max(xmax, x)
-				ymax = max(ymax, y)
-			if xmin == xmax and ymin == ymax: # Only one reference position
-				px = cp[0][0]
-			else:
-				std = max(xmax - xmin, ymax - ymin)
-				px = random.gauss((xmax + xmin) / 2, std)
-		py = line[0] * px + line[1]
-		return (px,py)
+    def __getrandompositiononline(self, line):
+        """Return a random position on a given line."""
+        cp = self.positions.values() # current positions for reference
+        if len(cp) == 0: # No reference positions
+            px = random.gauss(0, 1)
+        else:
+            xmin,ymin = xmax,ymax = cp[0]
+            for x,y in cp[1:]:
+                xmin = min(xmin, x)
+                ymin = min(ymin, y)
+                xmax = max(xmax, x)
+                ymax = max(ymax, y)
+            if xmin == xmax and ymin == ymax: # Only one reference position
+                px = cp[0][0]
+            else:
+                std = max(xmax - xmin, ymax - ymin)
+                px = random.gauss((xmax + xmin) / 2, std)
+        py = line[0] * px + line[1]
+        return (px,py)
 
-	def __getlinefromrule(self, dot, rule):
-		"""For a dot, each rule can restrict it to be on a line.
-		This method calculates a set of parameters (a,b) from a rule,
-		such that the dot (x,y) must be on y=ax+b."""
-		type = gettype(rule)
-		if type is TYPES['ANGLE']:
-			if len(rule) == 3:
-				rotation = 180.0
-			else:
-				rotation = float(rule[4:])
-			if rule[0] is dot:
-				baseline = rule[1:3]	# 'ABC' -> 'BC'
-				basedot = rule[1]
-				rotation = -rotation
-			elif rule[2] is dot:
-				baseline = rule[1::-1]	# 'ABC' -> 'BA'
-				basedot = rule[1]
-			else:
-				logging.error('Invalid rule: ' + rule)
-				return
-		elif type is TYPES['PERP'] or type is TYPES['PARA']:
-			if type is TYPES['PERP']:
-				rotation = 90.0
-			else:
-				rotation = 0.0
-			if rule[0] is dot:
-				basedot = rule[1]
-				baseline = rule[3:5]
-			elif rule[1] is dot:
-				basedot = rule[0]
-				baseline = rule[3:5]
-			elif rule[3] is dot:
-				basedot = rule[4]
-				baseline = rule[0:2]
-			elif rule[4] is dot:
-				basedot = rule[3]
-				baseline = rule[0:2]
-			else:
-				logging.error('Invalid rule: ' + rule)
-				return
-		else:
-			logging.error('Unknown rule: ' + rule)
-			return
-		# Now we get baseline, basedot and rotation
-		x0,y0 = self.positions[basedot]
-		x1,y1 = self.positions[baseline[0]]
-		x2,y2 = self.positions[baseline[1]]
-		if x1 == x2:
-			if y2 > y1:
-				theta = math.pi/2
-			elif y2 < y1:
-				theta = -math.pi/2
-			else:
-				logging.error('Invalid rule (identical dots): ' + rule)
-				return
-		else:
-			theta = math.atan((y2 - y1) / (x2 - x1))
-		theta += rotation / 180.0 * math.pi
-		a = math.tan(theta)
-		b = y0 - a * x0
-		logging.info(basedot + dot + ': y=' + str(a) + 'x+' + str(b) \
-			+ ' (theta=' + str(theta / math.pi * 180) + ')')
-		return a,b
+    def __getlinefromrule(self, dot, rule):
+        """For a dot, each rule can restrict it to be on a line.
+        This method calculates a set of parameters (a,b) from a rule,
+        such that the dot (x,y) must be on y=ax+b."""
+        type = gettype(rule)
+        if type is TYPES['ANGLE']:
+            if len(rule) == 3:
+                rotation = 180.0
+            else:
+                rotation = float(rule[4:])
+            if rule[0] is dot:
+                baseline = rule[1:3]    # 'ABC' -> 'BC'
+                basedot = rule[1]
+                rotation = -rotation
+            elif rule[2] is dot:
+                baseline = rule[1::-1]  # 'ABC' -> 'BA'
+                basedot = rule[1]
+            else:
+                logging.error('Invalid rule: ' + rule)
+                return
+        elif type is TYPES['PERP'] or type is TYPES['PARA']:
+            if type is TYPES['PERP']:
+                rotation = 90.0
+            else:
+                rotation = 0.0
+            if rule[0] is dot:
+                basedot = rule[1]
+                baseline = rule[3:5]
+            elif rule[1] is dot:
+                basedot = rule[0]
+                baseline = rule[3:5]
+            elif rule[3] is dot:
+                basedot = rule[4]
+                baseline = rule[0:2]
+            elif rule[4] is dot:
+                basedot = rule[3]
+                baseline = rule[0:2]
+            else:
+                logging.error('Invalid rule: ' + rule)
+                return
+        else:
+            logging.error('Unknown rule: ' + rule)
+            return
+        # Now we get baseline, basedot and rotation
+        x0,y0 = self.positions[basedot]
+        x1,y1 = self.positions[baseline[0]]
+        x2,y2 = self.positions[baseline[1]]
+        if x1 == x2:
+            if y2 > y1:
+                theta = math.pi/2
+            elif y2 < y1:
+                theta = -math.pi/2
+            else:
+                logging.error('Invalid rule (identical dots): ' + rule)
+                return
+        else:
+            theta = math.atan((y2 - y1) / (x2 - x1))
+        theta += rotation / 180.0 * math.pi
+        a = math.tan(theta)
+        b = y0 - a * x0
+        logging.info(basedot + dot + ': y=' + str(a) + 'x+' + str(b) \
+            + ' (theta=' + str(theta / math.pi * 180) + ')')
+        return a,b
 
-	def __findintersect(self, lines):
-		"""Find the intersect point of two lines.
-		Each line is defined by (a,b) thus it would be y=ax+b."""
-		a1,b1 = lines[0]
-		a2,b2 = lines[1]
-		if (a1 == a2):
-			logging.warning('Oops! Parallel lines never intersect! Ignoring one rule')
-			return self.__getrandompositiononline(lines[0])
-		x = (b2 - b1) / (a1 - a2)
-		y = (a1 * b2 - a2 * b1) / (a1 - a2)
-		return (x,y)
+    def __findintersect(self, lines):
+        """Find the intersect point of two lines.
+        Each line is defined by (a,b) thus it would be y=ax+b."""
+        a1,b1 = lines[0]
+        a2,b2 = lines[1]
+        if (a1 == a2):
+            logging.warning('Oops! Parallel lines never intersect! Ignoring one rule')
+            return self.__getrandompositiononline(lines[0])
+        x = (b2 - b1) / (a1 - a2)
+        y = (a1 * b2 - a2 * b1) / (a1 - a2)
+        return (x,y)
 
-	def __calcpositions(self):
-		"""Calculate the positions of all dots in self.dots,
-		based on the rules in self.dotrules."""
-		for dot in self.dots:
-			freedom = 2
-			lines = []
-			rules = self.__getrulesfordot(dot)
-			logging.info(dot + ": " + str(rules))
-			for rule in rules:
-				type = gettype(rule)
-				if type is TYPES['POSITION']:
-					x,y = rule[1:-1].split(',')
-					pos = (float(x), float(y))
-					freedom = 0
-					if len(rules) > 1:
-						logging.warning('Position of ' + dot + ' is assigned. ' \
-							+ 'Other rules will be ignored')
-				else:
-					line = self.__getlinefromrule(dot, rule)
-					if line is not None:
-						lines.append(line)
-						freedom -= 1
-				# If overdetermined, additional rules would be ignored
-				if freedom == 0:
-					break
-			if freedom == 2:
-				# Not determined - generate a random position
-				pos = self.__getrandomposition()
-			elif freedom == 1:
-				# Underdetermined - generate a semi-random position 
-				pos = self.__getrandompositiononline(lines[0])
-			elif len(lines) == 2:
-				# Fully determined - calculate the exact position
-				pos = self.__findintersect(lines)
-			self.positions[dot] = pos
-			logging.info('Freedom: ' + str(freedom) + '. Position: ' + str(pos))
+    def __calcpositions(self):
+        """Calculate the positions of all dots in self.dots,
+        based on the rules in self.dotrules."""
+        for dot in self.dots:
+            freedom = 2
+            lines = []
+            rules = self.__getrulesfordot(dot)
+            logging.info(dot + ": " + str(rules))
+            for rule in rules:
+                type = gettype(rule)
+                if type is TYPES['POSITION']:
+                    x,y = rule[1:-1].split(',')
+                    pos = (float(x), float(y))
+                    freedom = 0
+                    if len(rules) > 1:
+                        logging.warning('Position of ' + dot + ' is assigned. ' \
+                            + 'Other rules will be ignored')
+                else:
+                    line = self.__getlinefromrule(dot, rule)
+                    if line is not None:
+                        lines.append(line)
+                        freedom -= 1
+                # If overdetermined, additional rules would be ignored
+                if freedom == 0:
+                    break
+            if freedom == 2:
+                # Not determined - generate a random position
+                pos = self.__getrandomposition()
+            elif freedom == 1:
+                # Underdetermined - generate a semi-random position
+                pos = self.__getrandompositiononline(lines[0])
+            elif len(lines) == 2:
+                # Fully determined - calculate the exact position
+                pos = self.__findintersect(lines)
+            self.positions[dot] = pos
+            logging.info('Freedom: ' + str(freedom) + '. Position: ' + str(pos))
 
 def main():
-	if len(sys.argv) == 2:
-		infile = sys.argv[1]
-	else:
-		print 'USAGE: python ' + sys.argv[0] + ' FILENAME'
-		return
-	input = open(infile, 'r')
-	calc = SmartCalc(input)
-	print calc.positions
-	for line in calc.lines:
-		plt.plot([calc.positions[line[0]][0], calc.positions[line[1]][0]], \
-			[calc.positions[line[0]][1], calc.positions[line[1]][1]], '-ok')
-		plt.hold('on')
-	for dot in calc.dots:
-		plt.text(calc.positions[dot][0] + 0.03, calc.positions[dot][1] + 0.03, dot)
-	plt.axis('equal')
-	plt.axis('off')
-	plt.grid('on')
-	plt.show()
+    if len(sys.argv) == 2:
+        infile = sys.argv[1]
+    else:
+        print 'USAGE: python ' + sys.argv[0] + ' FILENAME'
+        return
+    input = open(infile, 'r')
+    calc = SmartCalc(input)
+    print calc.positions
+    for line in calc.lines:
+        plt.plot([calc.positions[line[0]][0], calc.positions[line[1]][0]], \
+            [calc.positions[line[0]][1], calc.positions[line[1]][1]], '-ok')
+        plt.hold('on')
+    for dot in calc.dots:
+        plt.text(calc.positions[dot][0] + 0.03, calc.positions[dot][1] + 0.03, dot)
+    plt.axis('equal')
+    plt.axis('off')
+    plt.grid('on')
+    plt.show()
 
 if __name__ == '__main__':
-	main()
+    main()
--- rules.py
+++ rules.py
@@ -1,55 +1,224 @@
-class Rule:
-	""""""
-	def __init__(self, description):
-		self.description = description
-		self.type = self.gettype()
-		self.shapes = self.initshapes()
+class RuleConstructor:
+    """Construct a rule and shapes from a string @definition.
 
-	def gettype(self):
-		return 'Base'
+    Attributes:
+        rule: a rule instance, or None if failed.
+        shapes: a list of shapes implicitly defined in @definition.
+    """
 
-	def initshapes(self):
-		""""""
-		return []
+    def __init__(self, description):
+        self.rule = None
+        self.shapes = []
+        if not description:
+            return
+        description = description.replace(' ', '')
+        if description is '':
+            return
+        if len(description) is 2:
+            self.shapes = [description]  # TODO shape
+            return
+        for Type in RuleTypes:
+            if Type.bingo(description):
+                self.rule = Type(description)
+                self.shapes = self.rule.get_shapes()
 
-	@classmethod
-	def bingo(cls, description):
-		return cls.separator in description
 
-	def getlinefordot(self, dot, positions):
-		pass
+class Rule(object):
+    """Prototype for a rule class. Do NOT instantiate this class.
 
-	def draw(self):
-		pass
+    Attributes:
+        type: a string to describe the type of this rule.
+        description: a string to describe the rule.
+    """
+
+    type = 'base'  # Should be overriden by subclasses
+
+    @classmethod
+    def bingo(cls, description):
+        """Return True if the description string can be recognized to be
+        a valid rule of this type."""
+        pass
+
+    def __init__(self, description):
+        self.description = description
+        self.type = self.__class__.type
+
+    def get_shapes(self):
+        """Get the shapes implicitly defined in the rule."""
+        return []
+
+    def get_constraint_for_dot(self, dot, positions):
+        """Get the constraint for a specified dot in the rule,
+        provided the positions of all other dots."""
+        pass
+
+    @staticmethod
+    def are_float(strings):
+        """Return True if all the string in @strings are float numbers."""
+        for s in strings:
+            try:
+                float(s)
+            except ValueError:
+                return False
+        return True
+
+
+class RulePosition(Rule):
+    """Define the position of a dot.
+
+    e.g. '(3.1,2.5)'
+    """
+    type = 'position'
+    __wrap_l = '('
+    __wrap_r = ')'
+    __separator = ','
+
+    @classmethod
+    def bingo(cls, description):
+        """Return True if the description string can be recognized to be
+        a valid rule of this type."""
+        values = description[1:-1].split(cls.__separator)
+        return (description[0] is cls.__wrap_l and
+                description[-1] is cls.__wrap_r and
+                len(values) is 2 and
+                cls.are_float(values))
+
+    def get_constraint_for_dot(self, dot, positions):
+        """Get the constraint for a specified dot in the rule,
+        provided the positions of all other dots."""
+        pass
+
 
 class RulePara(Rule):
-	separator = '-'
-	def gettype(self):
-		return 'Para'
+    """Make two lines be parallel.
+
+    e.g. 'AB-CD'
+    """
+
+    type = 'para'
+    __separator = '-'
+
+    @classmethod
+    def bingo(cls, description):
+        """Return True if the description string can be recognized to be
+        a valid rule of this type."""
+        lines = description.split(cls.__separator)
+        return (len(lines) is 2 and
+                len(lines[0]) is 2 and
+                len(lines[1]) is 2)
+
+    def get_shapes(self):
+        """Get the shapes implicitly defined in the rule."""
+        lines = self.description.split(self.__class__.__separator)
+        return lines
+
+    def get_constraint_for_dot(self, dot, positions):
+        """Get the constraint for a specified dot in the rule,
+        provided the positions of all other dots."""
+        pass
+
 
 class RulePerp(Rule):
-	separator = '|'
-	def gettype(self):
-		return 'Para'
+    """Make two lines be perpendicular.
 
-class RuleConstructor:
-	"""Determine the type of rule from a rule definition and 
-	return a corresponding rule instance"""
-	def __call__(cls, description):
-		for Type in RuleTypes:
-			if Type.bingo(description):
-				return Type(description)
-		return None
+    e.g. 'AB|CD'
+    """
+
+    type = 'perp'
+    __separator = '|'
+
+    @classmethod
+    def bingo(cls, description):
+        """Return True if the description string can be recognized to be
+        a valid rule of this type."""
+        lines = description.split(cls.__separator)
+        return (len(lines) is 2 and
+                len(lines[0]) is 2 and
+                len(lines[1]) is 2)
+
+    def get_shapes(self):
+        """Get the shapes implicitly defined in the rule."""
+        lines = self.description.split(self.__class__.__separator)
+        return lines
+
+    def get_constraint_for_dot(self, dot, positions):
+        """Get the constraint for a specified dot in the rule,
+        provided the positions of all other dots."""
+        pass
+
+
+class RuleAngle(Rule):
+    """Define the value of a angle.
+
+    e.g. 'ABC=45'
+    """
+
+    type = 'angle'
+    __separator = '='
+
+    @classmethod
+    def bingo(cls, description):
+        """Return True if the description string can be recognized to be
+        a valid rule of this type."""
+        angle, _, value = description.partition(cls.__separator)
+        return (cls.__separator in description and
+                len(angle) is 3 and
+                cls.are_float([value]))
+
+    def get_shapes(self):
+        """Get the shapes implicitly defined in the rule."""
+        lines = [self.description[0:2], self.description[1:3]]
+        return lines
+
+    def get_constraint_for_dot(self, dot, positions):
+        """Get the constraint for a specified dot in the rule,
+        provided the positions of all other dots."""
+        pass
+
+
+class RuleCollinear(RuleAngle):
+    """Restrict three dots to be collinear.
+
+    e.g. 'ABC' (is equivalent to 'ABC=180')
+    """
+
+    @classmethod
+    def bingo(cls, description):
+        return len(description) is 3
+
+    def __init__(self, description):
+        self.description = description + '=180'
+        self.type = self.__class__.type
+
+
+RuleTypes = [RulePosition, RulePara, RulePerp, RuleAngle, RuleCollinear]
+
 
 def test():
-	descriptions = ['AB|CD', 'AB-CD', 'ABCD']
-	for d in descriptions:
-		print d
-		rule = RuleConstructor()(d)
-		if rule is not None:
-			print rule.type
-
-RuleTypes = [RulePara, RulePerp]
+    descriptions = ['(1.3, 2.4)',
+                    'AB-CD',
+                    'AB|CD',
+                    'ABC',
+                    'ABC=32',
+                    None,
+                    '',
+                    'AB',
+                    '(1.3a,2.4)',
+                    'ABC-EF',
+                    ' AB',
+                    'ABCD',
+                    'ABC=32a',
+                    'ABCD=32']
+    for d in descriptions:
+        print d
+        rule_constructor = RuleConstructor(d)
+        if rule_constructor.rule:
+            print '%s --> Type: %s. Shapes: %s' % (
+                rule_constructor.rule.description,
+                rule_constructor.rule.type,
+                rule_constructor.shapes)
+        else:
+            print '%s --> Invalid rule' % d
 
 if __name__ == '__main__':
-	test()
+    test()
